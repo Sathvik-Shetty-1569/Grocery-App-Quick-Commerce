b72218fb22dc8738b9c26d86c63d6345fe41a06f
Sathvik-Shetty-1569 <sathvikshetty189@gmail.com>
2025-06-26 16:57:54 +0530

    Billing order complete

--- src/features/order/BillDetails.tsx
+++ src/features/order/BillDetails.tsx
@@ -7,57 +7,95 @@ import { RFValue } from 'react-native-responsive-fontsize'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 
-const ReportItem:FC<{iconName: string, underline: boolean, title: string; price:number}> = ({iconName, underline, title, price}) => {
-    return(
-        <View style = {[styles.flexRowBetween,{marginBottom: 10}]}>
+const ReportItem: FC<{ iconName: string, underline?: boolean, title: string; price: number }> = ({ iconName, underline, title, price }) => {
+    return (
+        <View style={[styles.flexRowBetween, { marginBottom: 10 }]}>
             <View style={styles.flexRow}>
-            <Icon name={iconName} style={{opacity:0.7}} size={RFValue(12)}/>
-            <CustomText
-            style = {{textDecorationLine: underline ? 'underline' : 'none', textDecorationStyle: 'dashed'}}
-                variant ='h8'>{title}</CustomText>
+                <Icon name={iconName} style={{ opacity: 0.7 }} size={RFValue(12)} />
                 <CustomText
-                variant='h8'>{price}</CustomText>
+                    style={{ textDecorationLine: underline ? 'underline' : 'none', textDecorationStyle: 'dashed' }}
+                    variant='h8'>{title}</CustomText>
+
             </View>
+
+            <CustomText
+                variant='h8'>{price}</CustomText>
+        </View>
+    );
+
+};
+const BillDetails: FC<{ totalItemPrice: number }> = ({ totalItemPrice }) => {
+    return (
+        <View style={styles.container}>
+            <CustomText style={styles.text} fontFamily={Fonts.SemiBold}>
+                BillDetails
+            </CustomText>
+            <View>
+
+            </View>
+            <View style={styles.billContainer}>
+                <ReportItem
+                    iconName='article'
+                    title='Item Total'
+                    price={totalItemPrice}
+                />
+                <ReportItem
+                    iconName='pedal-bike'
+                    title='Delivery Charge'
+                    price={29} />
+                    <ReportItem
+                    iconName='shopping-bag'
+                    title='Handling Charge'
+                    price={2}
+                    />
+                <ReportItem
+                    iconName='cloudy-snowing'
+                    title='Surge charge'
+                    price={3}
+                />
+
+            </View>
+
+           <View style={[styles.flexRowBetween,{marginBottom: 15}]}>
+            <CustomText 
+            variant='h7'
+            style ={styles.text}
+            fontFamily={Fonts.SemiBold}>
+                Grand Total
+                </CustomText>
+              <CustomText style ={styles.text} fontFamily={Fonts.SemiBold}>
+                ₹{totalItemPrice + 34}
+                </CustomText>  
+                </View> 
+
         </View>
     )
-    
-}
-const BillDetails:FC<{totalItemPrice: number}> = ({totalItemPrice}) => {
-  return (
-    <View style={styles.container}>
-        <CustomText style={styles.text} fontFamily={Fonts.SemiBold}>
-            BillDetails
-        </CustomText>
-        <View style={styles.billContainer}></View>
-
-    </View>
-  )
 }
 
 export default BillDetails
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         backgroundColor: '#fff',
         borderRadius: 15,
         marginVertical: 15
     },
-    text:{
+    text: {
         marginHorizontal: 10,
         marginTop: 15
     },
-    billContainer:{
+    billContainer: {
         padding: 10,
         paddingBottom: 0,
         borderBottomColor: Colors.border,
         borderBottomWidth: 0.6
     },
-    flexRowBetween:{
+    flexRowBetween: {
         justifyContent: 'space-between',
         alignItems: 'center',
         flexDirection: 'row',
     },
-    flexRow:{
+    flexRow: {
         flexDirection: 'row',
         alignItems: 'center',
         gap: 5
