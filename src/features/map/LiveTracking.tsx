88989f629737c54c8a2d0742e7665f82dac81143
Sathvik-Shetty-1569 <sathvikshetty189@gmail.com>
2025-06-28 14:16:21 +0530

    Order Summary created

--- src/features/map/LiveTracking.tsx
+++ src/features/map/LiveTracking.tsx
@@ -1,14 +1,142 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { useAuthStore } from '@state/authStore'
+import { getOrderById } from '@service/orderService'
+import { Colors, Fonts } from '@utils/Constants'
+import LiveHeader from './LiveHeader'
+import { RFValue } from 'react-native-responsive-fontsize'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons' 
+import CustomText from '@components/ui/CustomText'
+import OrderSummary from './OrderSummary'
 
 const LiveTracking = () => {
+    const {currentOrder, setCurrentOrder} = useAuthStore()
+
+    const fetchOrderDetails = async () => {
+        const data = await getOrderById(currentOrder?._id as any)
+        setCurrentOrder(data);
+    }
+
+    useEffect(() => {
+        fetchOrderDetails
+    },[])
+
+    let msg = "Packing your order"
+    let time = "Arriving in 10 minutes"
+    if(currentOrder?.status === "confiremd"){
+        msg = "Arriving Soon"
+        time = "Arriving in 8 minutes"
+    }
+    else if(currentOrder?.status === "arriving"){
+        msg = "Order Picked up"
+        time = "Arriving in 6 minutes"
+    }
+    else if(currentOrder?.status === "delivered"){
+        msg = "Order Delivered"
+        time = "Fastest Delivery ⚡️"
+    }
+    
   return (
-    <View>
-      <Text>LiveTracking</Text>
+    <View style = {styles.container}>
+        <LiveHeader type = "Customer" title = {msg} secondTitle ={time}/>
+        <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollContent}
+        >
+            <View style = {styles.flexRow}>
+                <View style = {styles.iconContainer}>
+                    <Icon
+                    name = {currentOrder?.deliveryPartner ? 'phone' : 'shopping'}
+                    color = {Colors.disabled}
+                    size = {RFValue(20)}
+                    />
+                </View>
+                <View style ={{width : '82%'}}>
+                    <CustomText 
+                    numberOfLines={1}
+                    variant='h7'
+                    fontFamily={Fonts.SemiBold}
+                    >
+                        {currentOrder?.deliveryPartner?.name ||
+                        'We will soon assign delivery partner'}
+                    </CustomText>
+                    
+                    {currentOrder?.deliveryPartner && (
+                        <CustomText variant='h7' fontFamily={Fonts.Medium}>
+                            {currentOrder?.deliveryPartner?.phone}
+                        </CustomText>
+                    )}
+
+                    <CustomText variant='h9' fontFamily={Fonts.Medium}>
+                        {currentOrder?.deliveryPartner?
+                    'For Delivery instructions you can contact here': msg}    
+                    </CustomText>
+                </View>
+            </View>
+
+            <OrderSummary order={currentOrder}/>
+
+            <View style = {styles.flexRow}>
+                <View style = {styles.iconContainer}>
+                    <Icon name = 'cards-heart-outline' color = {Colors.disabled} size = {RFValue(20)} />
+                </View>
+
+                <View style = {{width : '82%'}}>
+                    <CustomText variant='h7' fontFamily={Fonts.SemiBold}>
+                        Do you like our app?
+                        </CustomText>
+                        <CustomText
+                        variant='h9'
+                        fontFamily={Fonts.Medium}
+                        >
+                            Give us 5 star rating
+                        </CustomText>
+                </View>
+                
+            </View>
+<CustomText
+fontFamily={Fonts.SemiBold}
+variant='h6'
+style = {{opacity: 0.6, marginTop: 20}}
+>Sathvik Shetty x SS Grocery Delivery App</CustomText>
+
+        </ScrollView>
     </View>
   )
 }
 
 export default LiveTracking
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container :{
+        flex: 1,
+        backgroundColor: Colors.secondary
+    },
+    scrollContent:{
+        paddingBottom: 150,
+        backgroundColor: Colors.backgroundSecondary,
+        padding: 15
+    },
+    flexRow:{
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+        width: "100%",
+        borderRadius: 15,
+        marginTop: 15,
+        paddingVertical: 10,
+        backgroundColor: "#fff",
+        padding: 10,
+        borderBottomWidth: 0.7,
+        borderColor : Colors.border
+    },
+    iconContainer:{
+        backgroundColor: Colors.backgroundSecondary,
+        padding: 10,
+        borderRadius: 100,
+        alignItems: "center",
+        justifyContent: "center"
+
+    
+    }
+})
